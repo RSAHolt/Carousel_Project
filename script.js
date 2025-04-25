3360a2d0bdd3df5f616388c688243fbce7b4647c
RSAHolt <lukejsmart@gmail.com>
2025-04-25 09:15:12 +0200

    backup

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // load dom first before allowing functions.
 $(document).ready(function() {
-  v
-    $("#btn").click(function() {
-      alert("Hello from local jQuery!");
-    });
-  
+    let index = 0;
 
+    function showSlide(i) {
+      const totalSlides = $('.item').length;
+        index=0;
+        index = (i + totalSlides) % totalSlides; // wrap around
+        const offset = -index * 100;
+        $('.slides').css('transform', `translateX(${offset}%)`);
+
+      
+    console.log(index,totalSlides,offset);
+
+    }
+  
+    //next and prev buttons
+    $('#next').on('click', () => showSlide(index + 1));
+    $('#prev').on('click', () => showSlide(index - 1));
+  
+    // auto-play
+    setInterval(() => showSlide(index + 1), 5000);
+    
 });
