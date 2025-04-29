e8f0168c9a299d16ba41cbbe0387225693a7dc7c
RSAHolt <lukejsmart@gmail.com>
2025-04-29 12:28:52 +0200

    updated css

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // load dom first before allowing functions.
 $(document).ready(function() {
     let index = 0;
+    $('.item').eq(index).find('.description').addClass('active');
 
+    //main carousel loop
     function showSlide(i) {
       const totalSlides = $('.item').length;
         index=0;
-        index = (i + totalSlides) % totalSlides; // wrap around
+        index = (i + totalSlides) % totalSlides;
         const offset = -index * 100;
+        //animation translations:
+
+        //for slides
         $('.slides').css('transform', `translateX(${offset}%)`);
+        
+        //for slide descriptions
+        $('.description').removeClass('active');
+        $('.item').eq(index).find('.description').addClass('active'); 
+        
+        //for slide indicator
+        $('.current-slide').text(index + 1);
+         $('.total-slides').text(totalSlides);
+
 
       
     console.log(index,totalSlides,offset);
